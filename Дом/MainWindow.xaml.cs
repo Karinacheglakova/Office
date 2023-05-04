3fe32e928da1ea634388e9c3397af0680aa89826
student <student@PC-VOL-231-10>
2023-05-04 08:45:03 +0300

    Добавьте файлы проекта.

---
+++ Дом/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RoomLibrary;
+
+namespace Дом
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Room room = new Room();
+        LivingRoom livingRoom = new LivingRoom();
+        Office office = new Office();
+
+        List<Room> lstRoom = new List<Room>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BaddRoom_Click(object sender, RoutedEventArgs e)
+        {
+            Room room = new Room();
+            room.RoomLength = Convert.ToDouble(TBLengthR.Text);
+            room.RoomWidth = Convert.ToDouble(TBWidthR.Text);
+            lstRoom.Add(room);
+        }
+
+        private void BaddLivingRoom_Click(object sender, RoutedEventArgs e)
+        {
+            LivingRoom livingRoom = new LivingRoom();
+            livingRoom.RoomLength = Convert.ToDouble(TBLengthL.Text);
+            livingRoom.RoomWidth = Convert.ToDouble(TBWidthL.Text);
+            livingRoom.NumWin = Convert.ToInt32(TBNumW.Text);
+            lstRoom.Add(livingRoom);
+        }
+
+        private void BaddOffice_Click(object sender, RoutedEventArgs e)
+        {
+            Office office = new Office();
+            office.RoomLength = Convert.ToDouble(TBLengthO.Text);
+            office.RoomWidth = Convert.ToDouble(TBWidthO.Text);
+            office.NumSockets = Convert.ToInt32(TBNumS.Text);
+            lstRoom.Add(office);
+        }
+
+        private void BGetList_Click(object sender, RoutedEventArgs e)
+        {
+            ListRooms.Content= "";
+            foreach (Room r in lstRoom)
+            ListRooms.Content += r.Info() + "\n";
+        }
+    }
+}
